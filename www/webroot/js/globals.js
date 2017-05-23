60e157e86f1b25a08604d774e91ba81f458141d0
Carlos Romero <veracarlos66@hotmail.es>
2017-05-22 21:36:06 -0500

    qewr

--- www/webroot/js/globals.js
+++ www/webroot/js/globals.js
@@ -7,5 +7,5 @@ window.url = "https://taxiseguro2.local/";*/
 //window.server = "http://localhost/agendapp/php/";
 //window.url = "http://localhost/agendapp/";
 
-window.server = "http://192.168.1.88/agendapp/php/";
-window.url = "http://192.168.1.88/agendapp/";
+window.server = "http://192.168.1.75/agendapp/php/";
+window.url = "http://192.168.1.75/agendapp/";
