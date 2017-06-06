a822f00f64efe2d9e5347f75d42cce5065b72633
Carlos Romero <veracarlos66@hotmail.es>
2017-06-06 18:11:59 -0500

    :palm_tree:

--- www/controller/buscadorController.js
+++ www/controller/buscadorController.js
@@ -1,5 +1,15 @@
 myApp.onPageInit('PrincipalUser', function (page) {
 	javascript:window.history.forward(1);
+	document.onkeydown = function(e){
+  
+	    e=window.event;
+	    if(!window.event)return false;
+	 	if(e.keyCode == 37 && e.altKey==true )
+		{return false;} 
+		if((e.keyCode == 116 || e.keyCode == 82) && e.ctrlKey==true)
+		{return false;} 
+	    
+	}
 	$("#buscar").on('keyup', function(){
         var value = $(this).val();
         $.ajax({
