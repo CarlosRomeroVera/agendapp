08e27e96f9a7fb59f5a244d1e72ffe9884987f8b
Carlos Romero <veracarlos66@hotmail.es>
2017-06-12 13:38:12 -0500

    switon

--- www/webroot/js/globals.js
+++ www/webroot/js/globals.js
@@ -1,5 +1,8 @@
 // window.server = "http://localhost/agendapp/php/";
 // window.url = "http://localhost/agendapp/";
 
-window.server = "http://192.168.1.79/agendapp/php/";
-window.url = "http://192.168.1.79/agendapp/";
+// window.server = "http://192.168.1.79/agendapp/php/";
+// window.url = "http://192.168.1.79/agendapp/";
+
+window.server = "https://pservices.sm2consultores.com/pbuscador/php/";
+window.url = "https://pservices.sm2consultores.com/pbuscador/";
